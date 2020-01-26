d906b7c2c77a5b557fad626dd3ba0cf54b499bf3
antocecere <antocecere77@gmail.com>
2020-01-26 16:39:58 +0100

    Configuring CORS for basic authentication

--- src/main/java/com/virtualpairprogrammers/roombooking/config/BasicAuthConfig.java
+++ src/main/java/com/virtualpairprogrammers/roombooking/config/BasicAuthConfig.java
@@ -2,6 +2,7 @@ package com.virtualpairprogrammers.roombooking.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -20,6 +21,7 @@ public class BasicAuthConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "/api/basicAuth/**").permitAll()
                 .antMatchers("/api/basicAuth/**")
                 .hasRole("ADMIN")
                 .and().httpBasic();
